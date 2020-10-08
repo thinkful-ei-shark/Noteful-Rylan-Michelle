0f576b7cc7e1e01017e510440198161a6e6af6cd
Rylan Wassem <dryland28@gmail.com>
2020-10-08 17:38:38 -0400

    message

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -74,7 +74,7 @@ export default class AddNote extends Component {
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-      
+
         <NotefulForm onSubmit={this.handleSubmit}>
           <div className='field'>
             <label htmlFor='note-name-input'>
